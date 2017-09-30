b6822268b0ce538d2e0a342325393383a60dab20
Roman Kuznetsov <roman@kuznero.com>
2017-10-01 00:05:41 +0200

    Properly work with data volume

--- mediawiki/init.sh
+++ mediawiki/init.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$WG_SERVER" ]; then
-  sed -i "s/wgServer = \"\"/wgServer = \"${WG_SERVER//\//\\\/}\"/" /var/www/html/LocalSettings.php
-fi
+chown -R www-data:www-data /var/www/data
 
-if [ ! -z "$WG_SITENAME" ]; then
-  sed -i "s/wgSitename = \"\"/wgSitename = \"${WG_SITENAME//\//\\\/}\"/" /var/www/html/LocalSettings.php
-fi
-
-if [ ! -z "$WG_META_NAMESPACE" ]; then
-  sed -i "s/wgMetaNamespace = \"\"/wgMetaNamespace = \"${WG_META_NAMESPACE//\//\\\/}\"/" /var/www/html/LocalSettings.php
-fi
-
-if [ ! -z "$WG_LOGO" ]; then
-  sed -i "s/wgLogo = \"\"/wgLogo = \"${WG_LOGO//\//\\\/}\"/" /var/www/html/LocalSettings.php
-fi
-
-if [ ! -f "/var/www/data/wikidb.sqlite" ]; then
-  echo Generating default database files ...
-  cp -vr /var/www/data_init/* /var/www/data
-  chown -R www-data:www-data /var/www/data
+if [ -f "/var/www/data/LocalSettings.php" ]; then
+  echo Applying local settings ...
+  cp -v /var/www/data/LocalSettings.php /var/www/html
+  chown www-data:www-data /var/www/html/LocalSettings.php
   echo "done."
 fi
 
