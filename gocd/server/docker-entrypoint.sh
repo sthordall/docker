b2d9c974bcedb40af3bc3aed0fdd534df5fd2f71
Roman Kuznetsov <roman@kuznero.com>
2017-12-11 13:21:52 +0100

    Upgrade gocd-server image to v17.12.0

--- gocd/server/docker-entrypoint.sh
+++ gocd/server/docker-entrypoint.sh
@@ -20,21 +20,21 @@ try() { echo "$ $@" 1>&2; "$@" || die "cannot $*"; }
 
 VOLUME_DIR="/godata"
 
-# these 3 vars are used by `/go-server/server.sh`, so we export
-export SERVER_WORK_DIR="/go-working-dir"
-export GO_CONFIG_DIR="/go-working-dir/config"
-export STDOUT_LOG_FILE="/go-working-dir/logs/go-server.out.log"
 
 # no arguments are passed so assume user wants to run the gocd server
 # we prepend "/go-server/server.sh" to the argument list
 if [[ $# -eq 0 ]] ; then
-	set -- /go-server/server.sh "$@"
+  set -- /go-server/server.sh "$@"
 fi
 
 # if running go server as root, then initialize directory structure and call ourselves as `go` user
 if [ "$1" = '/go-server/server.sh' ]; then
 
   if [ "$(id -u)" = '0' ]; then
+    export SERVER_WORK_DIR="/go-working-dir"
+    export GO_CONFIG_DIR="/go-working-dir/config"
+    export STDOUT_LOG_FILE="/go-working-dir/logs/go-server.out.log"
+
     server_dirs=(artifacts config db logs plugins addons)
 
     yell "Creating directories and symlinks to hold GoCD configuration, data, and logs"
@@ -48,7 +48,7 @@ if [ "$1" = '/go-server/server.sh' ]; then
     # ensure proper directory structure in the volume directory
     if [ ! -e "${VOLUME_DIR}" ]; then
       try mkdir "${VOLUME_DIR}"
-      try chown go:go "${SERVER_WORK_DIR}"
+      try chown go:go "${VOLUME_DIR}"
     fi
 
     for each_dir in "${server_dirs[@]}"; do
@@ -63,8 +63,31 @@ if [ "$1" = '/go-server/server.sh' ]; then
       fi
     done
 
-	  try exec /sbin/tini -- su-exec go "$0" "$@" >> ${STDOUT_LOG_FILE} 2>&1
+    if [ ! -e "${SERVER_WORK_DIR}/config/logback-include.xml" ]; then
+      try cp -rfv "/go-server/config/logback-include.xml" "${SERVER_WORK_DIR}/config/logback-include.xml"
+      try chown go:go "${VOLUME_DIR}/config/logback-include.xml"
+    fi
+
+    try install-gocd-plugins
+
+    yell "Running custom scripts in /docker-entrypoint.d/ ..."
+
+    # to prevent expansion to literal string `/docker-entrypoint.d/*` when there is nothing matching the glob
+    shopt -s nullglob
+
+    for file in /docker-entrypoint.d/*; do
+      if [ -f "$file" ] && [ -x "$file" ]; then
+        try "$file"
+      else
+        yell "Ignoring $file, it is either not a file or is not executable"
+      fi
+    done
+
+    try exec /sbin/tini -- su-exec go "$0" "$@"
   fi
 fi
+
+# these 3 vars are used by `/go-server/server.sh`, so we export
+export GO_SERVER_SYSTEM_PROPERTIES="${GO_SERVER_SYSTEM_PROPERTIES}${GO_SERVER_SYSTEM_PROPERTIES:+ }-Dgo.console.stdout=true"
 
 try exec "$@"
